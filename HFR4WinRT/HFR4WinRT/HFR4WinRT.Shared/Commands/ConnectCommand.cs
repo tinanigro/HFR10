cda5daaafc009061a98f7f6ee20e62acd25e13af
Thomas Nigro <tn@thomasnigro.fr>
2015-01-26 01:11:44 +0100

    Improve connect command

--- HFR4WinRT/HFR4WinRT/HFR4WinRT.Shared/Commands/ConnectCommand.cs
+++ HFR4WinRT/HFR4WinRT/HFR4WinRT.Shared/Commands/ConnectCommand.cs
@@ -1,4 +1,8 @@
-﻿using HFR4WinRT.Helpers;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using HFR4WinRT.Helpers;
+using HFR4WinRT.Model;
 using HFR4WinRT.Utilities;
 using HFR4WinRT.ViewModel;
 
@@ -8,7 +12,21 @@ namespace HFR4WinRT.Commands
     {
         public override async void Execute(object parameter)
         {
-            await Locator.Main.AccountManager.CurrentAccount.BeginAuthentication();
+            if (Locator.Main.AccountManager.Accounts.FirstOrDefault(x => x.Pseudo == Locator.Main.AccountManager.CurrentAccount.Pseudo) != null) return;
+            bool success = await Locator.Main.AccountManager.CurrentAccount.BeginAuthentication();
+            if (success)
+            {
+                Locator.Main.AccountManager.Accounts.Add(Locator.Main.AccountManager.CurrentAccount);
+                Locator.Main.AccountManager.AddCurrentAccountInDB();
+                await ThreadUI.Invoke(() =>
+                {
+                    Locator.NavigationService.Navigate(Page.Main);
+                });
+            }
+            else
+            {
+                Debug.WriteLine("Login failed");
+            }
         }
     }
 }
