ce64b2c1cace7e095e1462a611b3ff0ba512ae3f
Thomas Nigro <tn@thomasnigro.fr>
2015-01-26 02:32:39 +0100

    Add converters

---
+++ HFR4WinRT/HFR4WinRT/HFR4WinRT.Shared/Converters/AvatarIdToUrlConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace HFR4WinRT.Converters
+{
+    public class AvatarIdToUrlConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return "http://forum-images.hardware.fr/images/mesdiscussions-" + value + ".jpg";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
