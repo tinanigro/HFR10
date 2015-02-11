df01623355fa4a2c6211cc915cf52809885f4608
Thomas Nigro <tn@thomasnigro.fr>
2015-02-11 14:19:52 +0100

    Improve responsive design

--- HFR4WinRT/HFR4WinRT/HFR4WinRT.Shared/Views/MainPage.xaml.cs
+++ HFR4WinRT/HFR4WinRT/HFR4WinRT.Shared/Views/MainPage.xaml.cs
@@ -1,5 +1,11 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using HFR4WinRT.ViewModel;
 
 namespace HFR4WinRT.Views
 {
@@ -8,6 +14,47 @@ namespace HFR4WinRT.Views
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Window.Current.SizeChanged += CurrentOnSizeChanged;
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            Window.Current.SizeChanged -= CurrentOnSizeChanged;
+        }
+
+        private void CurrentOnSizeChanged(object sender, WindowSizeChangedEventArgs windowSizeChangedEventArgs)
+        {
+            if (windowSizeChangedEventArgs.Size.Width < 1050)
+            {
+                VisualStateManager.GoToState(this, Locator.Main.Topics.Any() ? "SnapTopicView" : "SnapTopicList", false);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "FullTwoColumns", false);
+            }
+        }
+
+        private void GoBack_OnClick(object sender, RoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "SnapTopicList", false);
+        }
+
+        private void Drapeaux_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            if (Window.Current.Bounds.Width < 1050)
+            {
+                VisualStateManager.GoToState(this, "SnapTopicView", false);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "FullTwoColumns", false);
+            }
         }
     }
 }
