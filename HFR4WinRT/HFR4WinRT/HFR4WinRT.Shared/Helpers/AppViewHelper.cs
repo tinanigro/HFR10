3d40329afc0bbeb7d8c923bc46001af357fbc91c
Thomas Nigro <tn@thomasnigro.fr>
2015-02-13 07:12:19 +0100

    Fix WP Reflection bug

--- HFR4WinRT/HFR4WinRT/HFR4WinRT.Shared/Helpers/AppViewHelper.cs
+++ HFR4WinRT/HFR4WinRT/HFR4WinRT.Shared/Helpers/AppViewHelper.cs
@@ -10,23 +10,26 @@ namespace HFR4WinRT.Helpers
 
         public static void SetAppView()
         {
+#if WINDOWS_APP
             var v = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
             var allProperties = v.GetType().GetRuntimeProperties();
             titleBar = allProperties.FirstOrDefault(x => x.Name == "TitleBar");
             if (titleBar == null) return;
             dynamic bb = titleBar.GetMethod.Invoke(v, null);
             bb.ExtendViewIntoTitleBar = true;
+#endif
         }
 
         public static void SetBackgroundButtonColor()
         {
+#if WINDOWS_APP
             var v = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
             var allProperties = v.GetType().GetRuntimeProperties();
             titleBar = allProperties.FirstOrDefault(x => x.Name == "TitleBar");
             if (titleBar == null) return;
             dynamic bb = titleBar.GetMethod.Invoke(v, null);
             bb.ButtonBackgroundColor = Colors.White;
-
+#endif
         }
     }
 }
