5c933cc8e6409ab055529fb55f00d8ab546b8ae6
Thomas Nigro <tn@thomasnigro.fr>
2015-04-06 15:36:45 +0200

    Move Pane code in Sidebar.xaml

---
+++ Hfr/Hfr/Views/ShellControls/Sidebar.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Hfr.Views.ShellControls
+{
+    public sealed partial class Sidebar : UserControl
+    {
+        public Sidebar()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
