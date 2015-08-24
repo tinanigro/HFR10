240857639afb91b660e222a387558d272ca8d882
FLKone <FLKone@users.noreply.github.com>
2015-08-24 17:23:09 +0200

    Added some debug URLs.

--- Hfr/Hfr/Utilities/HFRUrl.cs
+++ Hfr/Hfr/Utilities/HFRUrl.cs
@@ -6,5 +6,14 @@
         public static readonly string ConnectUrl = "/login_validation.php?config=hfr.inc";
         public static readonly string DrapFavUrl = "/forum1f.php?owntopic=1";
         public static readonly string ProfilePageUrl = "/user/editprofil.php?config=hfr.inc&page=5";
+
+        /* Debug URL */
+        public static readonly string Dbg_Form_QuoteSingleMPURL =           
+            "/message.php?config=hfr.inc&cat=prive&post=1975876&numrep=1970017756&ref=40&page=1&p=1&subcat=0&sondage=0&owntopic=0&new=0#formulaire";
+        public static readonly string Dbg_Form_QuoteSingleURL =             
+            "/message.php?config=hfr.inc&cat=23&post=26848&numrep=1545246&ref=10&page=2&p=1&subcat=540&sondage=0&owntopic=1&new=0#formulaire";
+        public static readonly string Dbg_Topic_FirstPageFlagThirdPostURL = 
+            "/forum2.php?config=hfr.inc&cat=23&subcat=540&post=26848&page=1&p=1&sondage=0&owntopic=1&trash=0&trash_post=0&print=0&numreponse=0&quote_only=0&new=0&nojs=0#t1545199";
+
     }
 }
