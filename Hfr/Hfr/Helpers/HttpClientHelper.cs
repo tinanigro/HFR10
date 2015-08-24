5c038a18dc1285e1e06917c1e01cb933007f10f7
FLKone <FLKone@users.noreply.github.com>
2015-08-24 21:59:54 +0200

    Post method added in HttpClientHelper.

--- Hfr/Hfr/Helpers/HttpClientHelper.cs
+++ Hfr/Hfr/Helpers/HttpClientHelper.cs
@@ -84,5 +84,67 @@ namespace Hfr.Helpers
             }
             return result;
         }
+
+        public static async Task<string> Post(string url, Dictionary<String, String> data)
+        {
+            if (Loc.Main.AccountManager.CurrentAccount != null)
+            {
+                return await Post(url, data, Loc.Main.AccountManager.CurrentAccount.CookieContainer);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<string> Post(string url, Dictionary<String, String> data, string cookieContainer)
+        {
+            string result = "";
+
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                try
+                {
+                    cleanCookies();
+                    Debug.WriteLine("Post Helper url = " + url);
+
+                    var baseAddress = new Uri("http://forum.hardware.fr");
+                    var cookieContainr = new CookieContainer();
+                    var handler = new HttpClientHandler();
+                    handler.CookieContainer = cookieContainr;
+                    handler.UseCookies = true;
+
+                    using (var client = new HttpClient(handler) { BaseAddress = baseAddress })
+                    {
+                        List<Cookie> entities = (List<Cookie>)JsonConvert.DeserializeObject(cookieContainer, typeof(List<Cookie>));
+                        var c = new CookieCollection();
+
+                        foreach (var entity in entities)
+                        {
+                            cookieContainr.Add(baseAddress, new Cookie(entity.Name, entity.Value));
+                        }
+
+                        var content = new FormUrlEncodedContent(new[]
+                        {
+                            new KeyValuePair<string, string>("", "login")
+                        });
+                        Debug.WriteLine("content = " + content);
+
+                        var resultObj = client.PostAsync(url, content).Result;
+                        resultObj.EnsureSuccessStatusCode();
+                        result = await resultObj.Content.ReadAsStringAsync();
+                        cleanCookies();
+
+                        Debug.WriteLine("Post Helper result = " + result);
+                    }
+
+                }
+                catch (Exception exception)
+                {
+                    Debug.WriteLine("HttpClientHelper.Post Exception : " + exception.ToString());
+                }
+            }
+            return result;
+        }
     }
 }
