241422f990949926ecc07a0c13ffdebd17a1c47e
Thomas Nigro <tn@thomasnigro.fr>
2016-01-02 04:21:43 +0100

    Add PrivateChat model

---
+++ Hfr/Hfr/Models/PrivateChat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hfr.Models
+{
+    public class PrivateChat
+    {
+        public string Subject { get; set; }
+        public string Poster { get; set; }
+        public bool NewMsg { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public string DateTimeString => DateTime.ToString("g", new CultureInfo("fr-FR"));
+    }
+}
