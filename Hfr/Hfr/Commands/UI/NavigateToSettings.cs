c945c33bcf69fde2f8e1700b9e1f517137d7c213
Thomas Nigro <tn@thomasnigro.fr>
2016-01-02 17:41:12 +0100

    Add NavigateToSettings command

---
+++ Hfr/Hfr/Commands/UI/NavigateToSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hfr.Model;
+using Hfr.Utilities;
+using Hfr.ViewModel;
+
+namespace Hfr.Commands.UI
+{
+    public class NavigateToSettings : Command
+    {
+        public override void Execute(object parameter)
+        {
+            Loc.NavigationService.Navigate(View.Settings);
+        }
+    }
+}
