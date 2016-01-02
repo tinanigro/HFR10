38c2a3434a8447482220e3f5dac80f83148850bf
Thomas Nigro <tn@thomasnigro.fr>
2016-01-02 04:19:39 +0100

    Add Responsive Design states width in Strings.cs

--- Hfr/Hfr/Utilities/Strings.cs
+++ Hfr/Hfr/Utilities/Strings.cs
@@ -10,5 +10,9 @@ namespace Hfr.Utilities
     public static class Strings
     {
         public static GridLength DefaultMargin => (GridLength) App.Current.Resources["DefaultMarginGridLength"];
+        public static double MinimumWidth => (double) App.Current.Resources[nameof(MinimumWidth)];
+        public static double PortraitWidth => (double)App.Current.Resources[nameof(PortraitWidth)];
+        public static double NormalWidth => (double)App.Current.Resources[nameof(NormalWidth)]; 
+        public static double WideWidth => (double) App.Current.Resources[nameof(WideWidth)];
     }
 }
