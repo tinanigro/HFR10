bd98f7f342b6f18b54bec90056a71721633b3d5d
Thomas Nigro <tn@thomasnigro.fr>
2016-01-02 04:21:57 +0100

    Add Subcategory model

---
+++ Hfr/Hfr/Models/SubCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Hfr.Models
+{
+    public class SubCategory
+    {
+        public string CategoryName { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
