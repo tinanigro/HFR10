68614127f6335dbb2f1a730912fab7553bfcef31
Thomas Nigro <tn@thomasnigro.fr>
2016-01-02 04:22:15 +0100

    Update UI

---
+++ Hfr/Hfr/Views/MainPages/MainPageControls/TopicList.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Hfr.Views.MainPages.MainPageControls
+{
+    public sealed partial class TopicList : UserControl
+    {
+        public TopicList()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SemanticZoom_OnViewChangeCompleted(object sender, SemanticZoomViewChangedEventArgs e)
+        {
+            DrapsZoomeOutListView.ItemsSource = DrapsCvs.View.CollectionGroups;
+        }
+    }
+}
---
+++ Hfr/Hfr/Views/MainPages/TopicView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Windows.UI.Xaml.Controls;
+using Hfr.ViewModel;
+namespace Hfr.Views.MainPages
+{
+    public sealed partial class TopicView : UserControl
+    {
+        public TopicView()
+        {
+            this.InitializeComponent();
+            Loc.Main.TopicReadyToBeDisplayed += CurrentTopic_TopicReadyToBeDisplayed;
+        }
+
+        private void CurrentTopic_TopicReadyToBeDisplayed(Model.Topic topic, string computedHtml)
+        {
+            TopicWebView.NavigateToString(computedHtml);
+        }
+
+        private void TopicWebView_OnNavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            if (args.Uri != null)
+            {
+                args.Cancel = true;
+                Debug.WriteLine("WW " + args.Uri.Query + "-- " + args.Uri + " -- " + args.Uri.AbsoluteUri);
+                string param = args.Uri.Query.Replace("?", "");
+                var viewModel = (MainViewModel)DataContext;
+                if (viewModel.ContextMessageCommand.CanExecute(param))
+                    viewModel.ContextMessageCommand.Execute(param);
+
+            }
+            else
+                Debug.WriteLine("WW initial =" + args);
+        }
+    }
+}
