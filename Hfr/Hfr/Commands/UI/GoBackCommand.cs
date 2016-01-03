a2f3af828bcf7b85cb54196bdb1ceb39a3f503d5
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 03:00:36 +0100

    Add GoBackCommand

---
+++ Hfr/Hfr/Commands/UI/GoBackCommand.cs
@@ -0,0 +1,13 @@
+﻿using Hfr.Utilities;
+using Hfr.ViewModel;
+
+namespace Hfr.Commands.UI
+{
+    public class GoBackCommand : Command
+    {
+        public override void Execute(object parameter)
+        {
+            Loc.NavigationService.GoBack();
+        }
+    }
+}
