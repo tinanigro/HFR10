8fe45a8289a64f8fbde031367b914126bff2df82
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 23:22:20 +0100

    Add GoToAppTopic command

---
+++ Hfr/Hfr/Commands/Settings/GoToAppTopicCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hfr.Model;
+using Hfr.Utilities;
+using Hfr.ViewModel;
+
+namespace Hfr.Commands.Settings
+{
+    public class GoToAppTopicCommand : Command
+    {
+        public override void Execute(object parameter)
+        {
+            Loc.NavigationService.Navigate(View.Main);
+            var topic = new Model.Topic();
+            topic.TopicCatId = 23;
+            topic.TopicDrapURI = "/hfr/gsmgpspda/tablette/tablettes-telephones-windows-sujet_26848_1.htm";
+            topic.TopicName = "[Topic Unique] HFR10";
+            //topic.TopicDrapURI = 
+            Loc.Main.OpenTopicCommand.Execute(topic);
+        }
+    }
+}
