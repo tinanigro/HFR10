70771d5edd6aa1594d70be13924d9d4710383408
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 23:13:33 +0100

    Handle different types of parameter

--- Hfr/Hfr/Commands/Editor/ShowEditorCommand.cs
+++ Hfr/Hfr/Commands/Editor/ShowEditorCommand.cs
@@ -10,7 +10,14 @@ namespace Hfr.Commands.Editor
         public override void Execute(object parameter)
         {
             Debug.WriteLine("ShowEditorCommand param=" + parameter);
-            Loc.NavigationService.Navigate(View.Editor, parameter);
+            if (parameter is Model.Topic)
+            {
+                Loc.NavigationService.Navigate(View.Editor, ((Model.Topic)parameter).TopicNewPostUriForm);
+            }
+            else if (parameter is string)
+            {
+                Loc.NavigationService.Navigate(View.Editor, parameter);
+            }
             Loc.NavigationService.ShowBackButtonIfCanGoBack();
         }
     }
