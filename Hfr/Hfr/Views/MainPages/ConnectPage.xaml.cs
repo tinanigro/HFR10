45d0b4add621380ae8df3955caa746aa4b23c003
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 23:23:07 +0100

    Go to Password box when pressing Enter in pseudobox

--- Hfr/Hfr/Views/MainPages/ConnectPage.xaml.cs
+++ Hfr/Hfr/Views/MainPages/ConnectPage.xaml.cs
@@ -1,17 +1,23 @@
-﻿using Windows.UI.Xaml.Controls;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 namespace Hfr.Views.MainPages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class ConnectPage : Page
     {
         public ConnectPage()
         {
             this.InitializeComponent();
+        }
+
+        private void PseudoTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                e.Handled = true;
+                PasswordTextBox.Focus(FocusState.Keyboard);
+            }
         }
     }
 }
