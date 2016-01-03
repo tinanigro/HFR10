c859526c26e0b6f667dd2fad65f6eac40bb65c0d
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 23:14:45 +0100

    Hide StatusBar on Mobile if orientation is Landscape

--- Hfr/Hfr/Helpers/AppViewHelper.cs
+++ Hfr/Hfr/Helpers/AppViewHelper.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI;
+﻿using System;
+using Windows.Graphics.Display;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 
 namespace Hfr.Helpers
 {
@@ -6,7 +9,30 @@ namespace Hfr.Helpers
     {
         public static void SetAppView()
         {
-            var v = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+            DisplayInformation.GetForCurrentView().OrientationChanged += AppViewHelper_OrientationChanged;
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+            {
+                var statusBar = StatusBar.GetForCurrentView();
+                statusBar.BackgroundColor = (Color?) App.Current.Resources["SystemAccentColor"];
+                statusBar.BackgroundOpacity = 1;
+            }
+        }
+
+        private static async void AppViewHelper_OrientationChanged(DisplayInformation sender, object args)
+        {
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+            {
+                var statusBar = StatusBar.GetForCurrentView();
+                if (sender.CurrentOrientation == DisplayOrientations.Landscape ||
+                    sender.CurrentOrientation == DisplayOrientations.LandscapeFlipped)
+                {
+                    await statusBar.HideAsync();
+                }
+                else
+                {
+                    await statusBar.ShowAsync();
+                }
+            }
         }
     }
 }
