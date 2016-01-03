6d18543f6fd2aba25784d24425fa14e988fb6995
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 02:50:03 +0100

    Move ShowEditorCommands in a specific folder

---
+++ Hfr/Hfr/Commands/Editor/ShowEditorCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Hfr.Model;
+using Hfr.Utilities;
+using Hfr.ViewModel;
+
+namespace Hfr.Commands.Editor
+{
+    public class ShowEditorCommand : Command
+    {
+        public override void Execute(object parameter)
+        {
+            Debug.WriteLine("ShowEditorCommand param=" + parameter);
+            Loc.NavigationService.Navigate(View.Editor, parameter);
+            Loc.NavigationService.ShowBackButtonIfCanGoBack();
+        }
+    }
+}
--- Hfr/Hfr/Commands/EditorCommands.cs
+++ Hfr/Hfr/Commands/EditorCommands.cs
@@ -5,15 +5,7 @@ using System.Diagnostics;
 
 namespace Hfr.Commands
 {
-    public class ShowEditorCommand : Command
-    {
-        public override void Execute(object parameter)
-        {
-            Debug.WriteLine("ShowEditorCommand param=" + parameter);
-            Loc.NavigationService.Navigate(View.Editor, parameter);
-            Loc.NavigationService.ShowBackButtonIfCanGoBack();
-        }
-    }
+    
 
     public class SubmitEditorCommand : Command
     {
