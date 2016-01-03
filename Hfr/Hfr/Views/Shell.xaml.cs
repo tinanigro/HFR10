dacd56709afcda3d677e8df3f943333b455fde69
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 23:25:43 +0100

    Automagically resize the Flyout depending on Window size

--- Hfr/Hfr/Views/Shell.xaml.cs
+++ Hfr/Hfr/Views/Shell.xaml.cs
@@ -1,16 +1,49 @@
-﻿using System;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace Hfr.Views
 {
     public sealed partial class Shell : UserControl
     {
+        private readonly double ExtraPaneDefaultHeight = 650;
+        private readonly double ExtraPaneDefaultWidth = 400;
         public Shell()
         {
             this.InitializeComponent();
+            this.SizeChanged += Shell_SizeChanged;
         }
-        
+
+        private void Shell_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Responsive();
+        }
+
+        void Responsive()
+        {
+            if (this.ActualHeight < ExtraPaneDefaultHeight || this.ActualWidth < ExtraPaneDefaultWidth)
+            {
+                if (ExtraPaneVisible)
+                {
+                    ExtraPaneContentPresenter.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    ExtraPaneContentPresenter.VerticalAlignment = VerticalAlignment.Stretch;
+                    ExtraPaneContentPresenter.Height = this.ActualHeight;
+                    ExtraPaneContentPresenter.Width = this.ActualWidth;
+                }
+            }
+            else
+            {
+                if (ExtraPaneVisible)
+                {
+                    ExtraPaneContentPresenter.HorizontalAlignment = HorizontalAlignment.Right;
+                    ExtraPaneContentPresenter.VerticalAlignment = VerticalAlignment.Bottom;
+                    ExtraPaneContentPresenter.Height = ExtraPaneDefaultHeight;
+                    ExtraPaneContentPresenter.Width = ExtraPaneDefaultWidth;
+                }
+            }
+        }
+
+        public bool ExtraPaneVisible => ExtraPaneContent != null;
+
         public object ExtraPaneContent
         {
             get { return (object)GetValue(ExtraPaneContentProperty); }
@@ -27,8 +60,9 @@ namespace Hfr.Views
 
         void UpdateExtraPaneContent()
         {
-            ExtraPaneGrid.Visibility = (ExtraPaneContent == null) ? Visibility.Collapsed : Visibility.Visible;
+            ExtraPaneGrid.Visibility = (ExtraPaneVisible) ? Visibility.Visible : Visibility.Collapsed;
             ExtraPaneContentPresenter.Content = ExtraPaneContent;
+            Responsive();
         }
     }
 }
