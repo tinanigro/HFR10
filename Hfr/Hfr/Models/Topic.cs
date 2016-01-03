c003d0ca50a90780952666e3c9e3638d3a1e20ae
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 02:53:40 +0100

    Add new properties and getters to Topic model

--- Hfr/Hfr/Models/Topic.cs
+++ Hfr/Hfr/Models/Topic.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Text;
+using Windows.UI.Xaml.Media;
 using GalaSoft.MvvmLight;
 using Hfr.ViewModel;
 
 namespace Hfr.Model
 {
-    
+
     public class Topic : ViewModelBase
     {
+        private string _url;
         private string _html;
-        private string _renderedHtml;
-
-
-        
+        private string _topicReponseId;
+        private int _topicCurrentPage = 1;
 
         public string TopicName { get; set; }
+        public string TopicAuthor { get; set; }
+        public bool TopicIsSticky { get; set; }
+        public bool TopicIsClosed { get; set; }
+
         public int TopicCatId { get; set; }
         public string TopicSubCatId { get; set; }
         public string TopicId { get; set; }
@@ -23,26 +31,76 @@ namespace Hfr.Model
         public string TopicLastPost { get; set; }
         public TimeSpan TopicLastPostTimeSpan { get; set; }
         public int TopicNbPage { get; set; }
-        public int TopicCurrentPage { get; set; }
-        public string TopicReponseId { get; set; }
+
+        public int TopicCurrentPage
+        {
+            get { return _topicCurrentPage; }
+            set { Set(ref _topicCurrentPage, value); }
+        }
+
+        public string TopicReponseId
+        {
+            get { return _topicReponseId?.Replace("rep", ""); }
+            set { _topicReponseId = value; }
+        }
+
+        public string TopicNewPostUriForm { get; set; }
+
         public int TopicIndexCategory { get; set; }
-        public string TopicDrapURI { get; set; }
 
-        public ObservableCollection<Post> Posts { get; set; }
-
+        public string TopicDrapURI
+        {
+            get
+            {
+                WwwFormUrlDecoder decoder = new WwwFormUrlDecoder(_url);
+                // Manually set the desired page
+                // For flags
+                var pageKeyValue = decoder.FirstOrDefault(x => x.Name == "page");
+                if (pageKeyValue != null)
+                {
+                    var page = pageKeyValue.Value;
+                    _url = _url.Replace($"&page={page}", $"&page={TopicCurrentPage}");
+                }
+                else
+                {
+                    // For standard topics
+                    // Standard case : url ends with "_1.htm"
+                    int lastUnderlineIndex = _url.LastIndexOf('_');
+                    _url = _url.Substring(0, lastUnderlineIndex);
+                    _url += $"_{TopicCurrentPage}.htm";
+                }
+                return _url;
+            }
+            set { _url = value; }
+        }
+        
         public string Html
         {
             get { return _html; }
             set { Set(ref _html, value); }
         }
 
-        public string RenderedHtml
+        public FontWeight FontWeight
         {
-            get { return _renderedHtml; }
-            set
+            get
             {
-                Set(ref _renderedHtml, value);
-                Loc.Main.UpdateTopicWebView(this, value);
+                return TopicIsSticky ? FontWeights.SemiBold : FontWeights.Normal;
+            }
+        }
+
+        public SolidColorBrush Foreground
+        {
+            get
+            {
+                if (TopicIsSticky)
+                {
+                    return (SolidColorBrush)App.Current.Resources["SystemControlHighlightAltListAccentMediumBrush"];
+                }
+                else if (TopicIsClosed)
+                {
+                    return (SolidColorBrush)App.Current.Resources["SystemControlForegroundBaseLowBrush"];
+                }
+                return new SolidColorBrush(Colors.WhiteSmoke);
             }
         }
     }
