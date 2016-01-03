bad683a90788a4b3547d25e2eec446f8a5bb0b99
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 23:22:02 +0100

    Add ToastHelper

---
+++ Hfr/Hfr/Helpers/ToastHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using NotificationsExtensions.Toasts;
+
+namespace Hfr.Helpers
+{
+    public static class ToastHelper
+    {
+        public static void Simple(string textline1, string title = "HFR10")
+        {
+            var content = new ToastContent();
+            content.Visual = new ToastVisual()
+            {
+                TitleText = new ToastText()
+                {
+                    Text = title,
+                },
+                BodyTextLine1 = new ToastText()
+                {
+                    Text = textline1,
+                }
+            };
+            content.Audio = new ToastAudio()
+            {
+                Src = new Uri("ms-winsoundevent:Notification.IM")
+            };
+
+            XmlDocument doc = content.GetXml();
+
+            // Generate WinRT notification
+            var toast = new ToastNotification(doc);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+    }
+}
