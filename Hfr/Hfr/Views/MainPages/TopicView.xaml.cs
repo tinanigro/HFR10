d543853968fb45883b4053e1200f2e4bbd9b7f2f
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 02:59:21 +0100

    Update TopicViewc

--- Hfr/Hfr/Views/MainPages/TopicView.xaml.cs
+++ Hfr/Hfr/Views/MainPages/TopicView.xaml.cs
@@ -1,6 +1,15 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls;
+using Windows.Web;
+using Hfr.Utilities;
 using Hfr.ViewModel;
+
 namespace Hfr.Views.MainPages
 {
     public sealed partial class TopicView : UserControl
@@ -8,25 +17,46 @@ namespace Hfr.Views.MainPages
         public TopicView()
         {
             this.InitializeComponent();
-            Loc.Main.TopicReadyToBeDisplayed += CurrentTopic_TopicReadyToBeDisplayed;
+            Loc.Topic.TopicReadyToBeDisplayed += CurrentTopic_TopicReadyToBeDisplayed;
         }
 
-        private void CurrentTopic_TopicReadyToBeDisplayed(Model.Topic topic, string computedHtml)
+        private void CurrentTopic_TopicReadyToBeDisplayed(Model.Topic topic)
         {
-            TopicWebView.NavigateToString(computedHtml);
+            TopicWebView.NavigationCompleted += TopicWebViewOnNavigationCompleted;
+            TopicWebView.Navigate(Strings.TopicPageCacheUri);
+        }
+
+        private async void TopicWebViewOnNavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            TopicWebView.NavigationCompleted -= TopicWebViewOnNavigationCompleted;
+            if (!string.IsNullOrEmpty(Loc.Topic.CurrentTopic.TopicReponseId))
+            {
+                await TopicWebView.InvokeScriptAsync("scrollTo", new string[1]
+                {
+                    Loc.Topic.CurrentTopic.TopicReponseId
+                });
+            }
         }
 
         private void TopicWebView_OnNavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
         {
-            if (args.Uri != null)
+            if (args.Uri != null && args.Uri.AbsoluteUri.StartsWith(Strings.LocalUriPrefix))
             {
                 args.Cancel = true;
-                Debug.WriteLine("WW " + args.Uri.Query + "-- " + args.Uri + " -- " + args.Uri.AbsoluteUri);
-                string param = args.Uri.Query.Replace("?", "");
-                var viewModel = (MainViewModel)DataContext;
-                if (viewModel.ContextMessageCommand.CanExecute(param))
-                    viewModel.ContextMessageCommand.Execute(param);
-
+                if (args.Uri.AbsoluteUri.Contains("quote"))
+                {
+                    var decoder = new WwwFormUrlDecoder(args.Uri.Query);
+                    var postId = decoder.FirstOrDefault(x => x.Name == "postId")?.Value;
+                    if (!string.IsNullOrEmpty(postId))
+                        Loc.Topic.ShowEditorCommand.Execute(Loc.Topic.CurrentTopic.TopicNewPostUriForm + $"&numrep={postId}");
+                }
+                else
+                {
+                    Debug.WriteLine("WW " + args.Uri.Query + "-- " + args.Uri + " -- " + args.Uri.AbsoluteUri);
+                    string param = args.Uri.Query.Replace("?", "");
+                    if (Loc.Main.ContextMessageCommand.CanExecute(param))
+                        Loc.Main.ContextMessageCommand.Execute(param);
+                }
             }
             else
                 Debug.WriteLine("WW initial =" + args);
