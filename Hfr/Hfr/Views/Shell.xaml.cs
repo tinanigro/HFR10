99f8d5ba93971be4aa207779e8b69eab91571e04
Thomas Nigro <tn@thomasnigro.fr>
2016-01-03 02:59:46 +0100

    Remove Hamburger Menu + SplitView

--- Hfr/Hfr/Views/Shell.xaml.cs
+++ Hfr/Hfr/Views/Shell.xaml.cs
@@ -1,6 +1,6 @@
-﻿using Windows.UI.Xaml.Controls;
-
-// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Hfr.Views
 {
@@ -9,6 +9,26 @@ namespace Hfr.Views
         public Shell()
         {
             this.InitializeComponent();
+        }
+        
+        public object ExtraPaneContent
+        {
+            get { return (object)GetValue(ExtraPaneContentProperty); }
+            set { SetValue(ExtraPaneContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ExtraPaneContentProperty = DependencyProperty.Register(nameof(ExtraPaneContent), typeof(object), typeof(Shell), new PropertyMetadata(null, PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var that = (Shell) dependencyObject;
+            that.UpdateExtraPaneContent();
+        }
+
+        void UpdateExtraPaneContent()
+        {
+            ExtraPaneGrid.Visibility = (ExtraPaneContent == null) ? Visibility.Collapsed : Visibility.Visible;
+            ExtraPaneContentPresenter.Content = ExtraPaneContent;
         }
     }
 }
