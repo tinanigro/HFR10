e0335274f220283cb958f322be37691925285d9f
Thomas Nigro <thomas.nigro@outlook.com>
2016-01-04 22:46:21 +0100

    Add Strings Helper

---
+++ Hfr/Hfr/Helpers/StringHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hfr.Helpers
+{
+    public static class StringHelper
+    {
+        public static string CleanFromWeb(this string text)
+        {
+            return WebUtility.HtmlDecode(text.Trim());
+        }
+    }
+}
