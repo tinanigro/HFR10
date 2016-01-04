6ef74ca5471bba8272cd2074f1c6e179d8b8c4ba
Thomas Nigro <thomas.nigro@outlook.com>
2016-01-04 22:47:33 +0100

    Move Flag, Topic templates to standalone XAML files

---
+++ Hfr/Hfr/Views/Controls/FlagItemTemplate.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Hfr.Views.Controls
+{
+    public sealed partial class FlagItemTemplate : UserControl
+    {
+        public FlagItemTemplate()
+        {
+            this.InitializeComponent();
+        }
+
+        void RightTapped_Grid(object sender, RightTappedRoutedEventArgs args)
+        {
+            Flyout.ShowAttachedFlyout((Grid)sender);
+        }
+
+        private void Grid_Holding(object sender, HoldingRoutedEventArgs e)
+        {
+            Flyout.ShowAttachedFlyout((Grid)sender);
+        }
+    }
+}
---
+++ Hfr/Hfr/Views/Controls/StandardTopicItemTemplate.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Hfr.Views.Controls
+{
+    public sealed partial class StandardTopicItemTemplate : UserControl
+    {
+        public StandardTopicItemTemplate()
+        {
+            this.InitializeComponent();
+        }
+
+        void RightTapped_Grid(object sender, RightTappedRoutedEventArgs args)
+        {
+            Flyout.ShowAttachedFlyout((Grid)sender);
+        }
+
+        private void Grid_Holding(object sender, HoldingRoutedEventArgs e)
+        {
+            Flyout.ShowAttachedFlyout((Grid)sender);
+        }
+    }
+}
