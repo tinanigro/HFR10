443063bd57be2f974ccd32ca0fb0b38f51eb413a
Thomas Nigro <thomas.nigro@outlook.com>
2016-01-05 01:39:52 +0100

    Add Blank Page

---
+++ Hfr/Hfr/Views/MiscPages/BlankPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Hfr.Views.MiscPages
+{
+    public sealed partial class BlankPage : Page
+    {
+        public BlankPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
