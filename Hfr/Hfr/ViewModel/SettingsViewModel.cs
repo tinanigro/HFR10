7e7a64cdb2223983e675ebc650108a6906543062
Thomas Nigro <thomas.nigro@outlook.com>
2016-01-05 01:37:27 +0100

    Add Mail column visibility toggle in Settings

--- Hfr/Hfr/ViewModel/SettingsViewModel.cs
+++ Hfr/Hfr/ViewModel/SettingsViewModel.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GalaSoft.MvvmLight;
+﻿using GalaSoft.MvvmLight;
 using Hfr.Commands.Settings;
+using Hfr.Helpers;
 
 namespace Hfr.ViewModel
 {
     public class SettingsViewModel : ViewModelBase
     {
+        #region private properties
+        private bool _displayPrivateChatsByDefault;
+        #endregion
+        #region public properties
+        public bool DisplayPrivateChatsByDefault
+        {
+            get
+            {
+                var displayChats = ApplicationSettingsHelper.ReadSettingsValue(nameof(DisplayPrivateChatsByDefault), false);
+                if (displayChats == null)
+                {
+                    _displayPrivateChatsByDefault = true;
+                }
+                else
+                {
+                    _displayPrivateChatsByDefault = (bool)displayChats;
+                }
+                return _displayPrivateChatsByDefault;
+            }
+            set
+            {
+                if (_displayPrivateChatsByDefault == value) return;
+                ApplicationSettingsHelper.SaveSettingsValue(nameof(DisplayPrivateChatsByDefault), value, false);
+                Set(ref _displayPrivateChatsByDefault, value);
+            }
+        }
+        #endregion
+
+
         #region commands
         public GoToAppTopicCommand GoToAppTopicCommand { get; } = new GoToAppTopicCommand();
         #endregion
